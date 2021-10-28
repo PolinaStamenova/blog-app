4ad247ae4eeff68e64cf8f0b02381a36dfaf4c5f
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-28 16:44:56 +0300

    Create migrations to add columns to users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_133225) do
+ActiveRecord::Schema.define(version: 2021_10_28_134345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_10_28_133225) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -49,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_10_28_133225) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
