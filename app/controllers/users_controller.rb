600d0bc0778a70c2a009b0ef7eb42b67cba4c393
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-03 22:47:55 +0200

    Add instance variables: in index method to store all users in show method to store user by id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
-require 'pry'
 class UsersController < ApplicationController
   def index
-    # @users = User.all
+    @users = User.all
     # @current_user = current_user
   end
 
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
