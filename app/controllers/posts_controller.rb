bd693e48f83f0715998e7b9602f916e3322c10d4
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-04 19:59:02 +0200

    Add posts#new and posts#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
@@ -8,5 +9,26 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
     @post = Post.find(params[:id])
+  end
+
+  def new
+    # @post = Post.new(user_id: @user.id)
+    # @post = current_user.posts.build
+    @user = current_user
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
