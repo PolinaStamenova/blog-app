f7a40f6dcd4ddc9b877086bde79a823d2f728c11
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-04 21:06:24 +0200

    Add likes# and likes#create to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'liks/new'
+  get 'liks/create'
   root 'users#index'
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
     end
   end
 
