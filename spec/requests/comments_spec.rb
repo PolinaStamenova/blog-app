38962b1fa5a859d2ee94a3c628d10a2831471633
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-05 10:57:30 +0200

    Add CommentController

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/comments/create"
-      expect(response).to have_http_status(:success)
-    end
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
   end
-
 end
