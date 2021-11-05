6b5d6f3b2e7a68762dd97dcb4fcd2afb25aba34b
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-05 17:03:30 +0200

    Make add comments section works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,23 @@
+require "pry"
+
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
-    @comment.user_id = current_user.id
+    # @comment = @post.comments.build(comment_params)
+    @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+
+    # @comment.user_id = current_user.id
 
 
-   if @comment.save
-      redirect_to user_post_path(user_id: @post.user.id, post_id: @post.id)
+    # binding.pry
+  #  if @comment.save
+      # redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+      redirect_back(fallback_location: root_path)
       # redirect_to root_path
-   else
+  #  else
       # render 'new'
-      redirect_to root_path
-   end
+      # redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+  #  end
  end
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] # do
-      # resources :comments, only: [:create]
-    # end
+    resources :posts, only: [:index, :show, :new, :create]
   end
 
   resources :posts do
