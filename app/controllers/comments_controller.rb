344250e6db079e4417f68100923fe74e020f358f
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-05 17:12:46 +0200

    Refactor CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,22 +3,12 @@ require "pry"
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    # @comment = @post.comments.build(comment_params)
     @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
 
-    # @comment.user_id = current_user.id
-
-
-    # binding.pry
-  #  if @comment.save
-      # redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
-      redirect_back(fallback_location: root_path)
-      # redirect_to root_path
-  #  else
-      # render 'new'
-      # redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
-  #  end
+    redirect_back(fallback_location: root_path)
  end
+
+ private
 
   def comment_params
     params.require(:comment).permit(:text)
