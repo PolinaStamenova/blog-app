51f505c9ce327fd340933b84f377e42095d1b0b5
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-05 10:47:48 +0200

    Delete files

--- app/helpers/liks_helper.rb
+++
@@ -1,2 +0,0 @@
-module LiksHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      # resources :likes, only: [:new, :create]
     end
   end
 
