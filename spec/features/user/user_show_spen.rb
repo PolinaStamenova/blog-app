c510e9734b5345d688b7d17c00fbc71f0ce7ef68
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-10 13:29:59 +0200

    Cerate test for user#show view

---
+++ spec/features/user/user_show_spen.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "User show", type: :feature do
+  include Devise::Test::IntegrationHelpers
+  describe "User" do
+    let(:user) { User.create(name: "Polina", email: 'polina@example.com', password: 'password') }
+
+    before do
+      @user_one =  User.create(name: "Jordy", email: 'jordy@example.com', password: 'password', posts_counter: 3, bio: "This is my bio.")
+      @user_two =  User.create(name: "Marina", email: 'marina@example.com', password: 'password')
+
+      sign_in user
+      visit user_path(@user_one)
+    end
+
+
+    # it "should be able see profile picture for each user" do
+    #   # expect(page).to have_selector("img[src=testimonial-avatar-150x150]")
+    #   # expect(find('img').each { |img| img[:src] == 'https://avatarfiles.alphacoders.com/892/89214.gif' }).to be_truthy
+    # end
+
+    it "should be able see user's username" do
+      expect(page).to have_content("Jordy")
+    end
+
+    it "should be able see the number of posts the user has written." do
+      expect(page).to have_content("Number of posts:3")
+    end
+
+    it "should be able see the user's bio." do
+      expect(page).to have_content("This is my bio.")
+    end
+
+    # it "should redirected to that user's show page, when click on a user" do
+    #   find_link("user_#{@user_one.id}").click
+    #   expect(current_path).to eq(user_path(@user_one.id))
+    # end
+  end
+end
